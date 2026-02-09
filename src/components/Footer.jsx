df1196b04e090d72870b8e9c232a19a799e35e0a
Anil <puttaanilkumar666@gmail.com>
2026-02-09 17:59:33 -0600

    Update footer with social link emojis

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,8 @@ const Footer = () => {
     <footer className='text-center mb-4 text-grayish-cyan'>
       <p className='mb-1'>&copy; Anil Kumar - 2026</p>
       <p className='mb-1'>
-        <a href="http://linkedin.com/in/anil-putta" target="_blank" rel="noreferrer" className='underline mr-3'>LinkedIn</a>
+        <a href="https://linkedin.com/in/anil-putta" target="_blank" rel="noreferrer" className='underline mr-3'>🔗 LinkedIn</a>
+        <a href="https://github.com/Anilkumarputta" target="_blank" rel="noreferrer" className='underline mr-3'>🐙 GitHub</a>
         <a href="/LICENSE" className='underline'>License</a>
       </p>
     </footer>
